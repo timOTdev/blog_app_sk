285d8e8f0bba9ef936f9064b12ae694d3e26b9c2
timOTdev <tim@timothyhoang.dev>
2022-07-02 09:19:16 -0400

    Add component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,31 @@
-z
+<script>
+  let title = 'My Sveltekit Page';
+
+  const updateTitle = () => {
+    title = 'My Awesome Sveltekit Page';
+  };
+</script>
+
+<div class="index">
+  <h2>{title}</h2>
+  <p>
+    Building an app with all the modern best practices is fiendishly
+    complicated. Those practices include build optimizations, so that you load
+    only the minimal required code; offline support; prefetching pages before
+    the user initiates navigation; and configurable rendering that allows you to
+    generate HTML on the server or in the browser at runtime or at build-time.
+    SvelteKit does all the boring stuff for you so that you can get on with the
+    creative part
+  </p>
+
+  <button on:click={updateTitle}>Update Title</button>
+  <input type="text" bind:value={title} />
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
