cb081db9b00891a65e646f19beb01fb47b18c117
timOTdev <tim@timothyhoang.dev>
2022-07-03 14:34:09 -0400

    Add fetch call

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -1,8 +1,40 @@
+<script context="module">
+  // load() is a special sveltekit function to get data.
+  export async function load(context) {
+    // Can also destructure for {fetch}.
+    const res = await context.fetch(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    const guides = await res.json();
+    // context.fetch() contains ok and status for use.
+    if (res.ok) {
+      return {
+        props: {
+          guides,
+        },
+      };
+    }
+    return {
+      status: res.status,
+      error: new Error('Could not fetch the guides'),
+    };
+  }
+</script>
+
+<script>
+  export let guides;
+</script>
+
 <div class="guides">
   <h2>Svelte Guides</h2>
   <ul>
     <li><a href="/guides/guide1">Guide 1</a></li>
     <li><a href="/guides/guide2">Guide 2</a></li>
+    {#each guides as guide (guide.id)}
+      <li>
+        <a href="/">{guide.title}</a>
+      </li>
+    {/each}
   </ul>
 </div>
 
